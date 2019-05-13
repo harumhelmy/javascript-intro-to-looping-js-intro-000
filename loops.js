8d83b19f90e22290ba12915acd2b0ae7dcf93b46
Harum Helmy <harumendhah@gmail.com>
2019-05-13 14:08:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-  if (i == 1) {
-    array.push("I am 1 strange loop.");
-  } else {
-    array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
+    if (i == 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`);
+    }
   }
- }
- return array
+  return array
 }
 
 function whileLoop(n) {
